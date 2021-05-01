00a7d7a160b51f8f0ea54957e2325112d610c096
Adrian Lapierre <al@alapierre.io>
2021-05-01 12:05:59 +0200

    security patch for all images

--- build-all-java-images.sh
+++ build-all-java-images.sh
@@ -10,12 +10,14 @@ declare RED='\033[0;31m'
 declare NC='\033[0m' # No Color
 
 (cd jdk-8 && make multiarch)
+(cd jdk-11 && make multiarch)
 (cd jdk-14 && make multiarch)
 (cd jdk-15 && make multiarch)
-#(cd jdk-16 && make build push)
+(cd jdk-16 && make multiarch)
 
 
 trivy "$IMAGE_NAME":8
+trivy "$IMAGE_NAME":11
 trivy "$IMAGE_NAME":14
 trivy "$IMAGE_NAME":15
 trivy "$IMAGE_NAME":16
