bf5fc3ee7255800eb45935e30e1dca959b30d9e7
Adrian Lapierre <al@alapierre.io>
2021-12-05 11:29:28 +0100

    security update (MEDIUM: 4, HIGH: 18) - new version of JDK 17, 15, 11, 8 - new version of base alpine image 3.15.0

--- build-all-java-images.sh
+++ build-all-java-images.sh
@@ -14,6 +14,7 @@ declare NC='\033[0m' # No Color
 (cd jdk-14 && make multiarch)
 (cd jdk-15 && make multiarch)
 (cd jdk-16 && make multiarch)
+(cd jdk-17 && make multiarch)
 
 
 trivy "$IMAGE_NAME":8
@@ -21,3 +22,4 @@ trivy "$IMAGE_NAME":11
 trivy "$IMAGE_NAME":14
 trivy "$IMAGE_NAME":15
 trivy "$IMAGE_NAME":16
+trivy "$IMAGE_NAME":17
