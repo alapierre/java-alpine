a68af2339723b864224d3f37786e5159b4cf448b
Adrian Lapierre <al@alapierre.io>
2022-01-17 10:49:11 +0100

    security update - CVE-2021-42378 - CVE-2021-2388 - CVE-2021-45960 - CVE-2021-42379 - CVE-2021-42380 - CVE-2021-42381 - CVE-2021-42382 - CVE-2021-42383 - CVE-2021-42384 - CVE-2021-42385 - CVE-2021-42386 - CVE-2021-42374 - CVE-2021-42375 - CVE-2021-42374

--- build-all-java-images.sh
+++ build-all-java-images.sh
@@ -17,9 +17,9 @@ declare NC='\033[0m' # No Color
 (cd jdk-17 && make multiarch)
 
 
-trivy "$IMAGE_NAME":8
-trivy "$IMAGE_NAME":11
-trivy "$IMAGE_NAME":14
-trivy "$IMAGE_NAME":15
-trivy "$IMAGE_NAME":16
-trivy "$IMAGE_NAME":17
+trivy image "$IMAGE_NAME":8
+trivy image "$IMAGE_NAME":11
+trivy image "$IMAGE_NAME":14
+trivy image "$IMAGE_NAME":15
+trivy image "$IMAGE_NAME":16
+trivy image "$IMAGE_NAME":17
